03f64d1e65a15b2f35e2f56d751622f7441b8f9e
김벽걸 <biob1121@naver.com>
2022-06-02 01:46:25 +0900

    :feat Character (IMG) 이미지 파일 import
    캐릭터 이미지를 import하여 Array를 만들었다.

---
+++ src/assets/imgTest.js
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+
+import Jakie from "../assets/img/재키.png";
+import Hyunwoo from "../assets/img/현우.png";
+import Aya from "../assets/img/아야.png";
+import Magnus from "../assets/img/매그너스.png";
+import Fiora from "../assets/img/피오라.png";
+import Nadin from "../assets/img/나딘.png";
+import Zahir from "../assets/img/자히르.png";
+import Hart from "../assets/img/하트.png";
+import Isol from "../assets/img/아이솔.png";
+import LiDailin from "../assets/img/리다이린.png";
+import Yuki from "../assets/img/유키.png";
+import Hyejin from "../assets/img/혜진.png";
+import Xyowoo from "../assets/img/쇼우.png";
+import Sissela from "../assets/img/시셀라.png";
+import Chiara from "../assets/img/키아라.png";
+import Adriana from "../assets/img/아드리아나.png";
+import Silvia from "../assets/img/실비아.png";
+import Shoichi from "../assets/img/쇼이치.png";
+import Emma from "../assets/img/엠마.png";
+import Lenox from "../assets/img/레녹스.png";
+import Rozzi from "../assets/img/로지.png";
+import Loke from "../assets/img/루크.png";
+import Cathy from "../assets/img/캐시.png";
+import Adela from "../assets/img/아델라.png";
+import Bernice from "../assets/img/버니스.png";
+import Barbara from "../assets/img/바바라.png";
+import Alex from "../assets/img/알렉스.png";
+import Sua from "../assets/img/수아.png";
+import Leon from "../assets/img/레온.png";
+import Eleven from "../assets/img/일레븐.png";
+import Rio from "../assets/img/리오.png";
+import William from "../assets/img/윌리엄.png";
+import Nicky from "../assets/img/니키.png";
+import Nathapon from "../assets/img/나타폰.png";
+import Yan from "../assets/img/얀.png";
+import Eva from "../assets/img/이바.png";
+import Daniel from "../assets/img/다니엘.png";
+import Jennny from "../assets/img/제니.png";
+import Camilo from "../assets/img/카밀로.png";
+import Chloe from "../assets/img/클로에.png";
+import Johann from "../assets/img/요한.png";
+import Bianca from "../assets/img/비앙카.png";
+import Celine from "../assets/img/셀린.png";
+import Echion from "../assets/img/에키온.png";
+import Mai from "../assets/img/마이.png";
+import Aiden from "../assets/img/에이든.png";
+import Laura from "../assets/img/라우라.png";
+import Tia from "../assets/img/띠아.png";
+import Felix from "../assets/img/펠릭스.png";
+import Elena from "../assets/img/엘레나.png";
+import Priya from "../assets/img/프리야.png";
+import Adina from "../assets/img/아디나.png";
+import Markus from "../assets/img/마커스.png";
+
+export const nArray = [
+  Jakie,
+  Hyunwoo,
+  Aya,
+  Magnus,
+  Fiora,
+  Nadin,
+  Zahir,
+  Hart,
+  Isol,
+  LiDailin,
+  Yuki,
+  Hyejin,
+  Xyowoo,
+  Sissela,
+  Chiara,
+  Adriana,
+  Silvia,
+  Shoichi,
+  Emma,
+  Lenox,
+  Rozzi,
+  Loke,
+  Cathy,
+  Adela,
+  Bernice,
+  Barbara,
+  Alex,
+  Sua,
+  Leon,
+  Eleven,
+  Rio,
+  William,
+  Nicky,
+  Nathapon,
+  Yan,
+  Eva,
+  Daniel,
+  Jennny,
+  Camilo,
+  Chloe,
+  Johann,
+  Bianca,
+  Celine,
+  Echion,
+  Mai,
+  Aiden,
+  Laura,
+  Tia,
+  Felix,
+  Elena,
+  Priya,
+  Adina,
+  Markus,
+];
+
+// export const n01 = Jakie;
+// export const n02 = Aya;
+// export const n03 = Hyunwoo;
+// export const n04 = Magnus;
+// export const n05 = Fiora;
+// export const n06 = Nadin;
