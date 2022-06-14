c8da09d5415d73ed840a9d98a29a779c23213c05
김벽걸 <80795917+kimbuckgur@users.noreply.github.com>
2022-06-14 10:40:47 +0900

    Update state.js

--- src/components/State/state.js
+++ src/components/State/state.js
@@ -4,7 +4,6 @@ export const ETR_Infomation = atom({
   key: "ETR_Infomation",
   default: {
     url: "https://open-api.bser.io",
-    API_key: "ymj3AvldYuaQaSnf3oT3S5MrxNAU1NvH4SDndXvS",
   },
 });
 
